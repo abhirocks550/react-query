055ea874469f8f1511fa8912b783235cc4d4c769
=Abhijit Kulkarni <=>
2023-09-19 11:47:41 +0530

    Added React Query to the project and rqSuperhero page.

--- src/App.jsx
+++ src/App.jsx
@@ -3,32 +3,37 @@ import "./App.css";
 import Home from "./components/Home.page";
 import SuperHero from "./components/SuperHero.page";
 import RqSuperHero from "./components/RqSuperHero.page";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
     <>
-      <Router>
-        <div>
-          <nav>
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/superhero">SuperHero</Link>
-              </li>
-              <li>
-                <Link to="/rqsuperhero">RQ SuperHero</Link>
-              </li>
-            </ul>
-          </nav>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/superhero" element={<SuperHero />} />
-            <Route path="/rqsuperhero" element={<RqSuperHero />} />
-          </Routes>
-        </div>
-      </Router>
+      <QueryClientProvider client={queryClient}>
+        <Router>
+          <div>
+            <nav>
+              <ul>
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li>
+                  <Link to="/superhero">SuperHero</Link>
+                </li>
+                <li>
+                  <Link to="/rqsuperhero">RQ SuperHero</Link>
+                </li>
+              </ul>
+            </nav>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/superhero" element={<SuperHero />} />
+              <Route path="/rqsuperhero" element={<RqSuperHero />} />
+            </Routes>
+          </div>
+        </Router>
+      </QueryClientProvider>
     </>
   );
 }
--- src/components/RqSuperHero.page.jsx
+++ src/components/RqSuperHero.page.jsx
@@ -1,5 +1,23 @@
+import axios from "axios";
+import { useQuery } from "react-query";
+
 function RQSuperHero() {
-  return <div>RQSuperHero</div>;
+  const { isLoading, data } = useQuery("super-heroes", () => {
+    return axios.get("http://localhost:4000/superheroes");
+  });
+
+  if (isLoading) {
+    return <h2>Loading...</h2>;
+  }
+
+  return (
+    <>
+      <h2>Super Heroes Page</h2>
+      {data?.data?.map((hero) => {
+        return <div key={hero.name}>{hero.name}</div>;
+      })}
+    </>
+  );
 }
 
 export default RQSuperHero;
