43a1496c482fb7a7c6561ac93f21abc80ae45669
=Abhijit Kulkarni <=>
2023-09-19 11:40:20 +0530

    Fetch Super Heroes from API endpoint.
    Added axios

--- src/components/SuperHero.page.jsx
+++ src/components/SuperHero.page.jsx
@@ -1,5 +1,29 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 function SuperHero() {
-  return <div>SuperHero</div>;
+  const [isLoading, setIsLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:4000/superheroes").then((res) => {
+      setData(res.data);
+      setIsLoading(false);
+    });
+  }, []);
+
+  if (isLoading) {
+    return <h2>Loading...</h2>;
+  }
+
+  return (
+    <>
+      <h2>Super Heroes Page</h2>
+      {data.map((hero) => {
+        return <div key={hero.name}>{hero.name}</div>;
+      })}
+    </>
+  );
 }
 
 export default SuperHero;
